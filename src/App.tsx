d5944be1ef18e272fb532bf3afa8ddcd422b7042
ozahid7 <ozahid-@student.1337.ma>
2025-10-29 12:27:07 +0100

    feat: setup pre commits with linting and formatting

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+	const _logoLink = 'https://vitejs.dev/';
+	return (
+		<>
+			<p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
+		</>
+	);
+}
+
+export default App;
