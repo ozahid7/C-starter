05c95110527f7b38e4c4d87eb692b9ac80717bd8
ozahid7 <ozahid-@student.1337.ma>
2025-10-31 17:44:40 +0100

    feat: add register page with submitting

--- src/routes/(auth)/register/-components/register-form.tsx
+++ src/routes/(auth)/register/-components/register-form.tsx
@@ -21,34 +21,43 @@ import {
 import { Input } from "@/components/ui/input"
 import PasswordInput from "@/components/password-input"
 
-const formSchema = z.object({
-	password: z
-		.string()
-		.min(8, { message: "Password must be at least 8 characters long" })
-		.regex(/[A-Z]/, {
-			message: "Password must contain at least one uppercase letter",
-		})
-		.regex(/[a-z]/, {
-			message: "Password must contain at least one lowercase letter",
-		})
-		.regex(/[0-9]/, {
-			message: "Password must contain at least one number",
-		}),
-	email: z.email("Invalid email address."),
-})
+const formSchema = z
+	.object({
+		email: z.email("Invalid email address."),
+		password: z
+			.string()
+			.min(8, { message: "Password must be at least 8 characters long" })
+			.regex(/[A-Z]/, {
+				message: "Password must contain at least one uppercase letter",
+			})
+			.regex(/[a-z]/, {
+				message: "Password must contain at least one lowercase letter",
+			})
+			.regex(/[0-9]/, {
+				message: "Password must contain at least one number",
+			}),
+		confirmPassword: z
+			.string()
+			.min(1, { message: "Please confirm your password" }),
+	})
+	.refine((data) => data.password === data.confirmPassword, {
+		message: "Passwords do not match",
+		path: ["confirmPassword"],
+	})
 
-type LoginFormType = z.infer<typeof formSchema>
+type RegisterFormType = z.infer<typeof formSchema>
 
-export function LoginForm() {
-	const form = useForm<LoginFormType>({
+export function RegisterForm() {
+	const form = useForm<RegisterFormType>({
 		resolver: zodResolver(formSchema),
 		defaultValues: {
 			email: "",
 			password: "",
+			confirmPassword: "",
 		},
 	})
 
-	function onSubmit(data: LoginFormType) {
+	function onSubmit(data: RegisterFormType) {
 		toast.success("You submitted the following values:", {
 			description: (
 				<pre className="bg-code text-code-foreground mt-2 w-[320px] overflow-x-auto rounded-md p-4">
@@ -68,13 +77,13 @@ export function LoginForm() {
 	return (
 		<Card className="w-full sm:max-w-md">
 			<CardHeader>
-				<CardTitle>Login</CardTitle>
+				<CardTitle>Register</CardTitle>
 				<CardDescription>
-					Enter your email and password to sign in to yours account.
+					Enter your email and password to create a new account.
 				</CardDescription>
 			</CardHeader>
 			<CardContent>
-				<form id="login-form" onSubmit={form.handleSubmit(onSubmit)}>
+				<form id="register-form" onSubmit={form.handleSubmit(onSubmit)}>
 					<FieldGroup>
 						<Controller
 							name="email"
@@ -89,6 +98,7 @@ export function LoginForm() {
 										id="login-email-input"
 										aria-invalid={fieldState.invalid}
 										placeholder="example@email.com"
+										autoComplete="email"
 									/>
 									{fieldState.invalid && (
 										<FieldError
@@ -120,6 +130,28 @@ export function LoginForm() {
 								</Field>
 							)}
 						/>
+						<Controller
+							name="confirmPassword"
+							control={form.control}
+							render={({ field, fieldState }) => (
+								<Field data-invalid={fieldState.invalid}>
+									<FieldLabel htmlFor="login-confirm-password-input">
+										Confirm Password
+									</FieldLabel>
+									<PasswordInput
+										{...field}
+										id="login-confirm-password-input"
+										aria-invalid={fieldState.invalid}
+										placeholder="*********"
+									/>
+									{fieldState.invalid && (
+										<FieldError
+											errors={[fieldState.error]}
+										/>
+									)}
+								</Field>
+							)}
+						/>
 					</FieldGroup>
 				</form>
 			</CardContent>
@@ -132,8 +164,8 @@ export function LoginForm() {
 					>
 						Reset
 					</Button>
-					<Button form="login-form" type="submit">
-						Login
+					<Button form="register-form" type="submit">
+						Register
 					</Button>
 				</Field>
 			</CardFooter>
--- src/routes/(auth)/register/route.tsx
+++ src/routes/(auth)/register/route.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router"
+import { RegisterForm } from "./-components/register-form"
 
 export const Route = createFileRoute("/(auth)/register")({
 	component: RouteComponent,
 })
 
 function RouteComponent() {
-	return <div>Hello "/_auth/register/"!</div>
+	return (
+		<div className="flex flex-1 flex-col chv">
+			<RegisterForm />
+		</div>
+	)
 }
