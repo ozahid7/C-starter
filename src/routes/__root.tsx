7ca5d399dc0d4e76226323d341a773d16de586b0
ozahid7 <ozahid-@student.1337.ma>
2025-11-02 17:43:49 +0100

    feat: cleaning work space

--- src/routes/__root.tsx
+++
@@ -1,8 +0,0 @@
-import { createRootRoute } from "@tanstack/react-router"
-import NotFoundPage from "@/components/not-found"
-import GlobalLayout from "@/layouts/global-layout"
-
-export const Route = createRootRoute({
-	component: GlobalLayout,
-	notFoundComponent: NotFoundPage,
-})
